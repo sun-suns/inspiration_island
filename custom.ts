d5cf215ca1b3396457366431e565ca2437361d22
sun-suns <155649987+sun-suns@users.noreply.github.com>
2024-12-11 22:37:03 +0900

    更新 pxt.json, custom.ts

---
+++ custom.ts
@@ -0,0 +1,18 @@
+
+
+//% weight=999
+//% color=#c71585
+//% icon="\uf1b3"
+//% block="体験（たいけん）"
+namespace inspiration_island {
+
+    //% weight=90
+    //% blockId=teleportToNext1
+    //% block="次（つぎ）の地点（ちてん）へテレポート"
+    export function teleportToNext1() {
+        /*player.teleport(world(-21, 0, 0));*/
+        player.execute("tp @s 87 145 207 -90 0");
+    }
+
+    
+}
